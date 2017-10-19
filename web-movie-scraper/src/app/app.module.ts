3b6af0ab069784f59e2c586003b80478ed75f726
Giovanni <giov_lm@hotmail.it>
2017-10-19 11:44:55 +0200

    search-page component

--- web-movie-scraper/src/app/app.module.ts
+++ web-movie-scraper/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchPageComponent } from './search-page/search-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPageComponent
   ],
   imports: [
     BrowserModule
